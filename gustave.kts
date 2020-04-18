8fe2f05eb5d9a88d4b3d44a9ee96dd733a121f5e
Martin Bonnin <martin@mbonnin.net>
2020-04-19 01:22:50 +0200

    add bootstrap header

--- gustave.kts
+++ gustave.kts
@@ -1,4 +1,11 @@
-#!/usr/bin/env kscript
+#!/bin/bash
+
+//usr/bin/env echo '
+/**** BOOTSTRAP kscript ****\'>/dev/null
+command -v kscript >/dev/null 2>&1 || curl -L "https://git.io/fpF1K" | bash 1>&2
+exec kscript $0 "$@"
+\*** IMPORTANT: Any code including imports and annotations must come after this line ***/
+
 
 @file:MavenRepository("mbonnin", "https://dl.bintray.com/mbonnin/default/")
 @file:DependsOn("net.mbonnin.gustave:gustave-cli:0.0.3")
